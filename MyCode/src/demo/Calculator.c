66237677b9246d4a33dbe088ff5a1337c60d2b60
speriasami76 <72700395+speriasami76@users.noreply.github.com>
2020-10-12 20:58:06 +0530

    Create Calculator.c

---
+++ MyCode/src/demo/Calculator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main()
+{
+int a , b;
+ printf (" Enter 1st number = ", &a);
+ printf ("\nEnter 2nd number = ", &b);\
+ 
+ printf ("Sum of those two numbers = %d, ", (a+b));
+ 
+ }
